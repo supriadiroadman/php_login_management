dbf076c9e20caa99f7190cc949b6a898bacbee35
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-19 23:54:16 +0700

    Database Connection

---
+++ app/Config/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Supriadi\BelajarPhpMvc\Config;
+
+class Database
+{
+    private static ?\PDO $pdo = null;
+
+    public static function getConnection(string $env = "test"): \PDO
+    {
+        if (self::$pdo == null) {
+            // Create new PDO
+            require_once __DIR__.'/../../config/database.php';
+            $config = getDatabaseConfig();
+            self::$pdo = new \PDO(
+                $config['database'][$env]['url'],
+                $config['database'][$env]['username'],
+                $config['database'][$env]['password'],
+            );
+        }
+
+        return self::$pdo;
+    }
+}
+var_dump(Database::getConnection());
