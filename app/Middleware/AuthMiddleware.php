ec2a4c50be6f5503f3e5fe84a6595f77b6760b2f
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-19 10:25:41 +0700

    Implement  Middleware

---
+++ app/Middleware/AuthMiddleware.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Supriadi\BelajarPhpMvc\Middleware;
+
+class AuthMiddleware implements Middleware
+{
+    function before(): void
+    {
+        session_start();
+        if (!isset($_SESSION['user'])) {
+            header('Location: login');
+            exit();
+        }
+    }
+}
