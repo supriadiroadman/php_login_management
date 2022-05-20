2df3bb2a85b3a8b4a9fa3551a3103060508c38f3
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-20 11:47:27 +0700

    View Template

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -8,28 +8,6 @@ class HomeController
 {
     function index(): void
     {
-        $model = [
-            'title' => 'Belajar PHP MVC',
-            'content' => 'Selamat belajar PHP MVC'
-        ];
-
-        //echo "HomeController.index()";
-        //require __DIR__.'/../View/Home/index.php';
-        View::render('Home/index', $model);
-    }
-
-    function hello(): void
-    {
-        echo "HomeController.hello()";
-    }
-
-    function world(): void
-    {
-        echo "HomeController.world()";
-    }
-
-    function about(): void
-    {
-        echo "Author: Supriadi Roadman";
+        View::render('Home/index', ['title' => 'PHP Login Management']);
     }
 }
