2e6ca9bb6b92d2fdde242f8bf54590b9b4480656
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-20 11:57:58 +0700

    Unit Test View Template

---
+++ tests/App/ViewTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Supriadi\BelajarPhpMvc\App;
+
+use PHPUnit\Framework\TestCase;
+
+class ViewTest extends TestCase
+{
+    public function testRender()
+    {
+        View::render('Home/index', ['title' => 'PHP Login Management']);
+
+        $this->expectOutputRegex('[PHP Login Management]');
+        $this->expectOutputRegex('[html]');
+        $this->expectOutputRegex('[body]');
+        $this->expectOutputRegex('[Login Management]');
+        $this->expectOutputRegex('[Login]');
+        $this->expectOutputRegex('[Register]');
+    }
+
+}
