8d824fb8b15605efd323f8fccab54da91cbb94dd
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-22 23:04:34 +0700

    Unit Test User Profile Update Repository

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -9,9 +9,13 @@ use Supriadi\BelajarPhpMvc\Domain\User;
 class UserRepositoryTest extends TestCase
 {
     private UserRepository $userRepository;
+    private SessionRepository $sessionRepository;
 
     protected function setUp(): void
     {
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+        $this->sessionRepository->deleteAll();
+
         $this->userRepository = new UserRepository(Database::getConnection());
         $this->userRepository->deleteAll();
     }
@@ -39,6 +43,24 @@ class UserRepositoryTest extends TestCase
         $user = $this->userRepository->findById('not found');
         self::assertNull($user);
 
+    }
+
+    public function testUpdate()
+    {
+        $user = new User();
+        $user->setId('adi');
+        $user->setName('Adi');
+        $user->setPassword('rahasia');
+        $this->userRepository->save($user);
+
+        $user->setName('Ganti nama');
+        $this->userRepository->update($user);
+
+        $result = $this->userRepository->findById($user->getId());
+
+        self::assertEquals($user->getId(), $result->getId());
+        self::assertEquals($user->getName(), $result->getName());
+        self::assertEquals($user->getPassword(), $result->getPassword());
     }
 
 
